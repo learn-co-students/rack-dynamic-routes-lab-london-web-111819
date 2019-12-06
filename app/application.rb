c6b1cf96dc8d132654c0db219bbb52e0bd040031
badogan <basri.dogan.71@gmail.com>
2019-12-06 18:52:29 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,18 @@
 class Application
 
-    @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+    # @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item =@@items.find{|i| i.name == item_name}
-        resp.write item.price
+
+      the_item = Item.all.find{|i|i.name == item_name}
+
+    #   if item =@@items.find{|i| i.name == item_name}
+    if the_item != nil
+        resp.write the_item.price
       else 
         resp.status = 400
         resp.write "Item not found"
@@ -19,5 +23,5 @@ class Application
     end
     resp.finish
   end
-  
+
 end
